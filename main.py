232e3d9a1cd8391ceee33d6948bb9a4d830297ee
Olga Kovaleva <kovaleva.olga.2004@gmail.com>
2023-10-08 15:45:55 +0300

    Создание функции добавления расписания, функции замены 4-го параметра на ФИО посетителя

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import sqlite3
 
 bot = telebot.TeleBot('6432420440:AAGQcNnopghQU9RWCRL_FwODBDUPIl9dTT8')
 
-#Создание БД с расписанием
+# Создание БД с расписанием
 database = sqlite3.connect('rasp.db')
-#Создание курсора
+# Создание курсора
 cursor = database.cursor()
 
+
 def rasp_create():
-    #Создание таблицы с расписанием
+    # Создание таблицы с расписанием
     cursor.execute("""CREATE TABLE IF NOT EXISTS classes (
         date text,
         napr text,
@@ -17,8 +18,9 @@ def rasp_create():
         visitor text
     )""")
 
+
 def prob_create():
-    #Создание таблицы с оплаченными пробными занятиями
+    # Создание таблицы с оплаченными пробными занятиями
     cursor.execute("""CREATE TABLE IF NOT EXISTS prob_classes (
         date_today text,
         napr text,
@@ -26,9 +28,23 @@ def prob_create():
         visitor text
     )""")
 
+
+def insert_rasp(date, napr, coach, visitor='-'):
+    # Добавление расписания
+    cursor.execute("INSERT INTO classes (date, napr, coach, visitor) VALUES (?, ?, ?, ?)", (date, napr, coach, visitor))
+
+def update_visitor(date, napr, coach, visitor):
+    # Замена 4-го параметра посетилеля на реального человека, когда он записывается
+    cursor.execute("UPDATE classes SET visitor = ? WHERE date = ? AND napr = ? AND coach = ? AND visitor = '-'",
+        (visitor, date, napr, coach))
+
+
 prob_create()
 
-#Вывод таблицы с расписанием
+# Удаление данных
+#cursor.execute("DELETE FROM classes")
+
+# Вывод таблицы с расписанием
 #cursor.execute("SELECT * FROM classes")
 #print(cursor.fetchall())
 
@@ -36,10 +52,8 @@ database.commit()
 database.close()
 
 
-
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, 'Тест')
 
-
-#bot.polling(none_stop=True)
+# bot.polling(none_stop=True)
