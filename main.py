6ef4c290e627fa71c01960f7b81229073a24d724
4217 Маркова А.А. <anaviele@mail.ru>
2023-10-09 14:15:11 +0300

    Реализация таблицы с пробными занятиями + теперь можно добавлять в таблицу столько записей о направлении, сколько выделено на него мест

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import telebot
 import sqlite3
+from datetime import datetime
 
 bot = telebot.TeleBot('6432420440:AAGQcNnopghQU9RWCRL_FwODBDUPIl9dTT8')
 
@@ -23,6 +24,7 @@ def prob_create():
     # Создание таблицы с оплаченными пробными занятиями
     cursor.execute("""CREATE TABLE IF NOT EXISTS prob_classes (
         date_today text,
+        date text,
         napr text,
         coach text,
         visitor text
@@ -35,18 +37,42 @@ def insert_rasp(date, napr, coach, visitor='-'):
 
 def update_visitor(date, napr, coach, visitor):
     # Замена 4-го параметра посетилеля на реального человека, когда он записывается
-    cursor.execute("UPDATE classes SET visitor = ? WHERE date = ? AND napr = ? AND coach = ? AND visitor = '-'",
-        (visitor, date, napr, coach))
+    cursor.execute("SELECT rowid FROM classes WHERE date = ? AND napr = ? AND coach = ? AND visitor = '-'",
+        (date, napr, coach))
+    row = cursor.fetchone()
+    cursor.execute("UPDATE classes SET visitor = ? WHERE rowid = ?",
+        (visitor, row[0]))
 
+def prob_classes(date, napr, coach, visitor):
+    # Добавление информации о записи на пробное занятие в таблицу
+    date_today = datetime.now().date()
+    cursor.execute("INSERT INTO prob_classes (date_today, date, napr, coach, visitor) VALUES (?, ?, ?, ?, ?)", (date_today, date, napr, coach, visitor))
 
-prob_create()
 
 # Удаление данных
 #cursor.execute("DELETE FROM classes")
 
+# Удаление таблицы
+#cursor.execute("DROP TABLE prob_classes")
+
+"""insert_rasp('01.10.23', 'Растяжка', 'Тренер')
+insert_rasp('01.10.23', 'Растяжка', 'Тренер')
+insert_rasp('01.10.23', 'Растяжка', 'Тренер')
+insert_rasp('01.10.23', 'Растяжка', 'Тренер')
+insert_rasp('01.10.23', 'Йога', 'Тренер')
+insert_rasp('01.10.23', 'Йога', 'Тренер')
+insert_rasp('01.10.23', 'Йога', 'Тренер')
+insert_rasp('01.10.23', 'Йога', 'Тренер')"""
+
+#update_visitor('01.10.23', 'Йога', 'Тренер', 'Инна')
+
 # Вывод таблицы с расписанием
-#cursor.execute("SELECT * FROM classes")
-#print(cursor.fetchall())
+cursor.execute("SELECT * FROM classes")
+print(cursor.fetchall())
+
+# Вывод таблицы с пробными занятиями
+cursor.execute("SELECT * FROM prob_classes")
+print(cursor.fetchall())
 
 database.commit()
 database.close()
