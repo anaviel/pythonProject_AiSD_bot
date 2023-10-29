73634eea9be4264754f1ef61b5764aacc6ba3fb1
Olga Kovaleva <kovaleva.olga.2004@gmail.com>
2023-10-29 23:09:40 +0300

    Сделаны два класса: Admin - для админа и User - для пользователей, чтобы разделить функционал использования бота разными людьми

--- main.py
+++ main.py
@@ -43,6 +43,7 @@ def insert_rasp(date, napr, coach, visitor='-'):
     database.commit()
 
 def update_visitor(date, napr, coach, visitor):
+    cursor = database.cursor()
     # Замена 4-го параметра посетилеля на реального человека, когда он записывается
     cursor.execute("SELECT rowid FROM classes WHERE date = ? AND napr = ? AND coach = ? AND visitor = '-'",
                    (date, napr, coach))
@@ -60,7 +61,8 @@ def prob_classes(date, napr, coach, visitor):
     database.commit()
 
 def rasp_show(date):
-   # Вывод расписания
+    # Вывод расписания
+    cursor = database.cursor()
     cursor.execute("SELECT DISTINCT napr, coach FROM classes WHERE date = ?", (date,))
     rows = cursor.fetchall()
     rasp_list = []
@@ -75,6 +77,8 @@ def rasp_show(date):
 # Удаление таблицы
 #cursor.execute("DROP TABLE clsses")
 
+# для себя
+#cursor.execute("DELETE FROM classes WHERE rowid = (SELECT MAX(rowid) FROM classes)")
 #Заполнение таблицы
 
 """
@@ -118,91 +122,93 @@ print(cursor.fetchall())
 
 database.commit()
 
+
+class Admin():
+    def __init__(self, user_id):
+        self.user_id = user_id
+
+    # "Добавление расписания"
+    def add_rasp(self, message, date, napr, coach):
+        insert_rasp(date, napr, coach)
+        bot.send_message(message.chat.id, f"В расписание добавилась запись со следующими параметрами:\nДата: {date}\nНаправление: {napr}\nТренер: {coach}")
+
+    # "Абонементы"
+    def subscription(self, message):
+        subscription(message)
+
+    # "Обновить прайс-лист"
+    def update_price_list(self, message):
+        update_price_list(message)
+
+class User():
+    def __init__(self, user_id):
+        self.user_id = user_id
+
+    # "Записаться"
+    def sign_up_for_training(self, message):
+        sign_up_for_training(message)
+
+    # "Отменить занятие"
+    def cancel_registration_for_training(self, message):
+        cancel_registration_for_training(message)
+
+    # "Личный кабинет"
+    def personal_account(self, message):
+        personal_account(message)
+
+
 name = ''
+var = 1269188609
+
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    button1 = types.KeyboardButton('Записаться')
-    button2 = types.KeyboardButton('Добавить расписание')
-    button3 = types.KeyboardButton('Отменить занятие')
-    button4 = types.KeyboardButton('Абонементы')
-    button5 = types.KeyboardButton('Личный кабинет')
-    button6 = types.KeyboardButton('Обновить прайс-лист')
-    markup.row(button1, button3, button5)
-    markup.row(button2, button4, button6)
-    bot.send_message(message.chat.id, 'Бот для фитнес-студии', reply_markup=markup)
-    if name == '':
-        bot.send_message(message.chat.id, 'Перед тем, как начать пользоваться ботом, пожалуйста, укажите свои полные фамилию, имя и отчество.')
-        bot.register_next_step_handler(message, new_name)
+    user_id = message.from_user.id
+    if user_id == var:
+        markup1 = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        button1 = types.KeyboardButton('Добавить расписание')
+        button2 = types.KeyboardButton('Абонементы')
+        button3 = types.KeyboardButton('Обновить прайс-лист')
+        markup1.row(button1, button2, button3)
+        bot.send_message(message.chat.id, 'Добро пожаловать, администратор!)', reply_markup=markup1)
+        if name == '':
+            bot.send_message(message.chat.id, 'Перед тем, как начать пользоваться ботом, пожалуйста, укажите свои полные фамилию, имя и отчество.')
+            bot.register_next_step_handler(message, new_name)
+    else:
+        markup2 = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        button1 = types.KeyboardButton('Записаться')
+        button3 = types.KeyboardButton('Отменить запись')
+        button5 = types.KeyboardButton('Личный кабинет')
+        markup2.row(button1, button3, button5)
+        bot.send_message(message.chat.id, 'Бот для фитнес-студии', reply_markup=markup2)
+        if name == '':
+            bot.send_message(message.chat.id, 'Перед тем, как начать пользоваться ботом, пожалуйста, укажите свои полные фамилию, имя и отчество.')
+            bot.register_next_step_handler(message, new_name)
 
 @bot.message_handler(content_types=['text'])
 def menu(message):
+    user_id = message.from_user.id
+    # если боту написал пользователь
+    if user_id != var:
+        user = User(user_id)
+        if message.text == 'Записаться':
+            user.sign_up_for_training(message)
+        elif message.text == 'Отменить запись':
+            user.cancel_registration_for_training(message)
+        elif message.text == 'Личный кабинет':
+            user.personal_account(message)
+    # если боту написал админ
+    elif user_id == var:
+        admin = Admin(user_id)
+        if message.text == 'Добавить расписание':
+            bot.send_message(message.chat.id, 'Введите сообщение в формате:\nдд-мм-гг_Направление_Тренер')
+            bot.register_next_step_handler(message, lambda msg: admin.add_rasp(msg, msg.text.split('_')[0], msg.text.split('_')[1], msg.text.split('_')[2]))
+        elif message.text == 'Абонементы':
+            admin.subscription(message)
+        elif message.text == 'Обновить прайс-лист':
+            bot.send_message(message.chat.id, 'Пожалуйста, отправьте изображение обновлённого прайс-листа.')
+            bot.register_next_step_handler(message, admin.update_price_list)
 
-    if message.text == 'Записаться':
-        markup = types.InlineKeyboardMarkup()
-        cursor.execute("SELECT DISTINCT date FROM classes")
-        dates_ = cursor.fetchall()
-        dates = []
-        for date in dates_:
-            for d in date:
-                dates.append(d)
-        for date in dates:
-            butt = 'date:' + date
-            button = types.InlineKeyboardButton(date, callback_data=butt)
-            markup.add(button)
-        bot.send_message(message.chat.id, 'Выберите день:', reply_markup=markup)
-    elif message.text == 'Добавить расписание':
-        bot.send_message(message.chat.id, 'Введите сообщение в формате:\nгггг-мм-дд_Направление_Тренер')
-        bot.register_next_step_handler(message, add_rasp)
-    elif message.text == 'Отменить занятие':
-        visitor = name
-        cursor.execute("SELECT date, napr FROM classes WHERE visitor = ?", (visitor,))
-        markup = types.InlineKeyboardMarkup()
-        dates_napr_ = cursor.fetchall()
-        dates_napr = []
-        for d_n in dates_napr_:
-            dates_napr.append(d_n)
-        for d_n in dates_napr:
-            date = d_n[0]
-            napr = d_n[1]
-            butt = 'date-napr_' + date + '_' + napr
-            name_butt = date + ' ' + napr
-            button = types.InlineKeyboardButton(name_butt, callback_data=butt)
-            markup.add(button)
-        bot.send_message(message.chat.id, 'Какое направление хотите отменить?', reply_markup=markup)
-    elif message.text == 'Абонементы':
-        cursor.execute("SELECT * FROM subscription_inf")
-        print(cursor.fetchall())
-        markup_subscription = types.InlineKeyboardMarkup()
-        button1 = types.InlineKeyboardButton('Добавить абонемент', callback_data='add_subscription')
-        button2 = types.InlineKeyboardButton('Пополнить абонемент', callback_data='replenish_subscription')
-        markup_subscription.add(button1, button2)
-        cursor.execute("SELECT visitor FROM subscription_inf WHERE subscription > 0")
-        kol = len(cursor.fetchall())
-        bot.send_message(message.chat.id, f'Количество активных абонентов: {kol}', reply_markup=markup_subscription)
-    elif message.text == 'Личный кабинет':
-        cursor.execute("SELECT subscription FROM subscription_inf WHERE visitor = ?",
-                       (name,))
-        result = cursor.fetchone()[0]
-        cursor.execute("SELECT date, napr FROM classes WHERE visitor = ?", (name,))
-        dates_napr_ = cursor.fetchall()
-        dates_napr = []
-        for d_n in dates_napr_:
-            dates_napr.append(d_n)
-        classes = ''
-        for d_n in dates_napr:
-            date = d_n[0]
-            napr = d_n[1]
-            classes = classes + '\n' + '    ' + date + '  ' + napr + '\n'
-        image_file = os.listdir("images")
-        if image_file:
-            image_path = os.path.join("images", image_file[0])
-            with open(image_path, 'rb') as photo:
-                bot.send_photo(message.chat.id, photo, caption=f'🤍Уважаемая, {name}!\n\n    Количество занятий на балансе Вашего абонемента: {result}\n\n🎀Занятия, на которые Вы записаны:\n{classes}')
-    elif message.text == 'Обновить прайс-лист':
-        bot.send_message(message.chat.id, 'Пожалуйста, отправьте изображение обновлённого прайс-листа.')
-        bot.register_next_step_handler(message, update_price_list)
 @bot.callback_query_handler(func=lambda callback: 'add_subscription' in callback.data)
 def callback_add_subscription(callback):
     bot.send_message(callback.message.chat.id, f'Введите ФИО нового посетителя и количество пополняемых занятий через запятую.\n\nПример: <b><i>Иванова Екатерина Александровна, 30</i></b>', parse_mode='HTML')
@@ -254,6 +260,7 @@ def callback_dates_show(callback):
 
 @bot.callback_query_handler(func=lambda callback: 'reg_' in callback.data)
 def callback_reg(callback):
+    cursor = database.cursor()
     data_parts = callback.data.split('_')
     date = data_parts[1]
     napr = data_parts[2]
@@ -270,17 +277,102 @@ def callback_reg(callback):
     database.commit()
     bot.send_message(callback.message.chat.id, f'Вы успешно записаны {date} на {napr}.\nС баланса Вашего абонемента было списано одно занятие.')
 
+
 def new_name(message):
     global name
     name = message.text
     bot.send_message(message.chat.id, 'Ваше имя успешно сохранено.')
 
-def add_rasp(message):
-    date = message.text.split('_')[0]
-    napr = message.text.split('_')[1]
-    coach = message.text.split('_')[2]
-    insert_rasp(date, napr, coach)
-    bot.send_message(message.chat.id, f"В расписание добавилась запись со следующими параметрами:\nДата: {date}\nНаправление: {napr}\nТренер: {coach}")
+#def add_rasp(message):
+#    date = message.text.split('_')[0]
+#    napr = message.text.split('_')[1]
+#    coach = message.text.split('_')[2]
+#    insert_rasp(date, napr, coach)
+#    bot.send_message(message.chat.id, f"В расписание добавилась запись со следующими параметрами:\nДата: {date}\nНаправление: {napr}\nТренер: {coach}")
+
+# функция, которая вызывается при нажатии пользователем на кнопку "Записаться"
+def sign_up_for_training(message):
+    markup = types.InlineKeyboardMarkup()
+    cursor.execute("SELECT DISTINCT date FROM classes")
+    dates_ = cursor.fetchall()
+    dates = []
+    for date in dates_:
+        for d in date:
+            dates.append(d)
+    for date in dates:
+        butt = 'date:' + date
+        button = types.InlineKeyboardButton(date, callback_data=butt)
+        markup.add(button)
+    bot.send_message(message.chat.id, 'Выберите день:', reply_markup=markup)
+    cursor.close()
+
+
+# функция, которая вызывается при нажатии пользователем на кнопку "Отметить запись"
+def cancel_registration_for_training(message):
+    visitor = name
+    cursor.execute("SELECT date, napr FROM classes WHERE visitor = ?", (visitor,))
+    markup = types.InlineKeyboardMarkup()
+    dates_napr_ = cursor.fetchall()
+    dates_napr = []
+    for d_n in dates_napr_:
+        dates_napr.append(d_n)
+    for d_n in dates_napr:
+        date = d_n[0]
+        napr = d_n[1]
+        butt = 'date-napr_' + date + '_' + napr
+        name_butt = date + ' ' + napr
+        button = types.InlineKeyboardButton(name_butt, callback_data=butt)
+        markup.add(button)
+    bot.send_message(message.chat.id, 'Какое направление хотите отменить?', reply_markup=markup)
+
+
+# функция, которая вызывается при нажатии пользователем на кнопку "Личный кабинет"
+def personal_account(message):
+    cursor.execute("SELECT subscription FROM subscription_inf WHERE visitor = ?",
+                   (name,))
+    result = cursor.fetchone()[0]
+    cursor.execute("SELECT date, napr FROM classes WHERE visitor = ?", (name,))
+    dates_napr_ = cursor.fetchall()
+    dates_napr = []
+    for d_n in dates_napr_:
+        dates_napr.append(d_n)
+    classes = ''
+    for d_n in dates_napr:
+        date = d_n[0]
+        napr = d_n[1]
+        classes = classes + '\n' + '    ' + date + '  ' + napr + '\n'
+    image_file = os.listdir("images")
+    if image_file:
+        image_path = os.path.join("images", image_file[0])
+        with open(image_path, 'rb') as photo:
+            bot.send_photo(message.chat.id, photo,
+                           caption=f'🤍Уважаемая, {name}!\n\n    Количество занятий на балансе Вашего абонемента: {result}\n\n🎀Занятия, на которые Вы записаны:\n{classes}')
+
+
+# функция, которая вызывается при нажатии админом на кнопку "Обновить прайс-лист"
+def update_price_list(message):
+    file_info = bot.get_file(message.photo[-1].file_id)
+    file_name = f"images/{file_info.file_id}.jpg"
+    if os.listdir("images"):
+        os.remove(os.path.join("images", os.listdir("images")[0]))
+    downloaded_file = bot.download_file(file_info.file_path)
+    with open(file_name, 'wb') as new_file:
+        new_file.write(downloaded_file)
+    bot.send_message(message.chat.id, "Прайс-лист успешно обновлён.")
+
+
+# функция, которая вызывается при нажатии админом на кнопку "Абонементы"
+def subscription(message):
+    cursor.execute("SELECT * FROM subscription_inf")
+    print(cursor.fetchall())
+    markup_subscription = types.InlineKeyboardMarkup()
+    button1 = types.InlineKeyboardButton('Добавить абонемент', callback_data='add_subscription')
+    button2 = types.InlineKeyboardButton('Пополнить абонемент', callback_data='replenish_subscription')
+    markup_subscription.add(button1, button2)
+    cursor.execute("SELECT visitor FROM subscription_inf WHERE subscription > 0")
+    kol = len(cursor.fetchall())
+    bot.send_message(message.chat.id, f'Количество активных абонентов: {kol}', reply_markup=markup_subscription)
+
 
 def add_subscription(message, callback):
     try:
@@ -292,6 +384,8 @@ def add_subscription(message, callback):
     except:
         bot.send_message(message.chat.id, "Данные введены некорректно. Попробуйте снова.")
         callback_add_subscription(callback)
+
+
 def replenish_subscription(message, callback):
     try:
         visitor, kol = message.text.split(', ')
@@ -310,17 +404,6 @@ def replenish_subscription(message, callback):
     except:
         bot.send_message(message.chat.id, "Данные введены некорректно. Попробуйте снова.")
         callback_add_subscription(callback)
-
-def update_price_list(message):
-
-    file_info = bot.get_file(message.photo[-1].file_id)
-    file_name = f"images/{file_info.file_id}.jpg"
-    if os.listdir("images"):
-        os.remove(os.path.join("images", os.listdir("images")[0]))
-    downloaded_file = bot.download_file(file_info.file_path)
-    with open(file_name, 'wb') as new_file:
-        new_file.write(downloaded_file)
-    bot.send_message(message.chat.id, "Прайс-лист успешно обновлён.")
 
 
 
